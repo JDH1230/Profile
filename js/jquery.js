9a2942fb55874a77309f6302197b63bad75d47e8
jeondaehyun <boydh0123@naver.com>
2023-01-13 17:41:28 +0900

    학원에서 작업

--- js/jquery.js
+++ js/jquery.js
@@ -16,7 +16,47 @@ $(function() {
 //scroll-top 
 $(function() {
     $( '.top' ).click( function() {
-        $( 'html, body' ).animate( { scrollTop : 0 }, 1000 );
+        $( 'html, body' ).animate( { scrollTop : 0 }, 3000 );
         return false;
     });
 })
+
+//link about
+$(function() {
+    $( '#about' ).click( function() {
+        $( 'html, body' ).animate( { scrollTop : 200 }, 1000 );
+        return false;
+    });
+})
+
+//link skill
+$(function() {
+    $( '#skill' ).click( function() {
+        $( 'html, body' ).animate( { scrollTop : 1000 }, 1000 );
+        return false;
+    });
+})
+
+//link project 
+$(function() {
+    if(window.innerWidth > 768) {
+        $( '#project' ).click( function() {
+            $( 'html, body' ).animate( { scrollTop : 1500 }, 1000 );
+            return false;
+        });
+    } else {
+        $( '#project' ).click( function() {
+            $( 'html, body' ).animate( { scrollTop : 3000 }, 1000 );
+            return false;
+        });
+    }
+})
+
+//link contact
+$(function() {
+    $( '#contact' ).click( function() {
+        $( 'html, body' ).animate( { scrollTop : 8000 }, 3000 );
+        return false;
+    });
+})
+
